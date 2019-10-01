a0e36b62aae4a5bc4832cddcd115bc85f3a862f6
sdoubleday <28907224+sdoubleday@users.noreply.github.com>
2019-10-01 02:47:52 -0400

    fix path refs

--- hooks/post_gen_project.bat
+++ hooks/post_gen_project.bat
@@ -1,3 +1,3 @@
-copy ".\dir_{{cookiecutter.delimiter}}\{{cookiecutter.databuilder_name}}.sql" ".\dir_{{cookiecutter.delimiter}}\{{cookiecutter.databuilder_name}}.ps1"
+copy ".\{{cookiecutter.databuilder_name}}.sql" ".\{{cookiecutter.databuilder_name}}.ps1"
 
-powershell -NoProfile -NoLogo -ExecutionPolicy Bypass -File ".\dir_{{cookiecutter.delimiter}}\{{cookiecutter.databuilder_name}}.ps1"
+powershell -NoProfile -NoLogo -ExecutionPolicy Bypass -File ".\{{cookiecutter.databuilder_name}}.ps1"

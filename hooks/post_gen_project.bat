24e56f9b53968e8ce0bc9875cef3d09c26b458b8
sdoubleday <28907224+sdoubleday@users.noreply.github.com>
2020-01-25 13:01:17 -0500

    reflect new file structure in bat file by NOT copying the sql file

--- hooks/post_gen_project.bat
+++ hooks/post_gen_project.bat
@@ -1,5 +1,3 @@
-copy ".\{{cookiecutter.databuilder_name}}.sql" ".\{{cookiecutter.databuilder_name}}.ps1"
-
 powershell -NoProfile -NoLogo -ExecutionPolicy Bypass -File ".\{{cookiecutter.databuilder_name}}.ps1"
 
 del ".\{{cookiecutter.databuilder_name}}.ps1"
